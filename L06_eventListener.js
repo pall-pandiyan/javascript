d66f29b1926589acc9b79e8e961865c13af0628e
Pall Pandiyan <pallpandiyan@gmail.com>
2022-04-24 18:51:59 +0530

    showed two ways to add event listener

--- L06_eventListener.js
+++ L06_eventListener.js
@@ -1,3 +1,8 @@
+document.addEventListener("keydown", doubleClickEvent);
+function doubleClickEvent() {
+    alert("Stop Pressing random keys!")
+}
+
 document.addEventListener("click", ()=> {
     alert("Stop Clicking Me!");
 })
