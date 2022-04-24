2f2010213495adffcfb1362c7470dad653eb17a2
Pall Pandiyan <pallpandiyan@gmail.com>
2022-04-24 16:37:17 +0530

    added output for reference

--- L04_array.js
+++ L04_array.js
@@ -12,3 +12,10 @@ console.log("splice(0,2): " + String(favNums).replaceAll(","," ",));
 console.log("find the index of 20: " + favNums.indexOf(20));
 
 console.log("slice(2,4) " + favNums.slice(2,4));
+
+// the output will be...
+// favNums Array: 1 5 10 13 15 17
+// After pushing 20: 1 5 10 13 15 17 20
+// splice(0,2): 10 13 15 17 20
+// find the index of 20: 4
+// slice(2,4) 15,17
