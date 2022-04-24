b3d7133672a56ffdd88b655b1d00f9787dfabce5
Pall Pandiyan <pallpandiyan@gmail.com>
2022-04-24 16:35:58 +0530

    a simple if else statement

---
+++ L05_ifElse.js
@@ -0,0 +1,11 @@
+totalSum = 100;
+
+if (totalSum > 49) {
+    console.log("Thank you shopping with us!\nHave a nice day!")
+} else {
+    console.log("Sorry! we cannot afford to ship to you for less than $50!")
+}
+
+// the output will be...
+// Thank you shopping with us!
+// Have a nice day!
