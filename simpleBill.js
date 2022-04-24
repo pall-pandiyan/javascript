4e922d7ff7a69a4f35b38f8d9bd9df6b089bd33b
Pall Pandiyan <pallpandiyan@gmail.com>
2022-04-24 16:31:57 +0530

    generated a simple bill

---
+++ simpleBill.js
@@ -0,0 +1,21 @@
+let shoppingList = [
+    // [description, qty, rate]
+    ["apples", 1, 10],
+    ["oranges", 2, 12],
+    ["eggs", 1, 15]
+]
+
+let sum = 0;
+console.log("Shopping List:");
+console.log();
+console.log("\t" + "Name" + "\t" + "Qty" + "\t" + "Rate" + "\t" + "Amount");
+console.log("\t" + "----" + "\t" + "---" + "\t" + "----" + "\t" + "------");
+shoppingList.forEach(item => {
+    item.push(item[1] * item[2]);
+    console.log("\t" + String(item).replaceAll(",", "\t"));
+    sum += item[3];
+});
+
+console.log("\t\t\t----------");
+console.log("\t\t\tSum \t$" + sum);
+console.log("\t\t\t----------");
