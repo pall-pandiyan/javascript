ec18707afbf227b3810c5e58ca11ee424da08daf
Pall Pandiyan <pallpandiyan@gmail.com>
2022-05-04 14:04:39 +0530

    added detailed explanation

--- L01_variables.js
+++ L01_variables.js
@@ -1,11 +1,19 @@
-let a;
-a = 10;
-let b=20;
+let a; // only declaring
+a = 10; // now defining value
+let b = 20; // declaring and defining in the same line
 
+var c = 30; // the old way to declare variable, the scope of var is the entire function, so developers usually use 'let' instead
+
+const PI = 3.14; // constant variable should define the value when declared (in the same line.) the value of constant variable cannot be changed.
+
+// all of these variable can be aceesed by calling its name, constant variable only have read access but others can be modified or rewritten.
 console.log(a);
-console.log(b);
-
-
+console.log(a + b);
+console.log((c += a - b));
+console.log(PI * a * b);
 // the output will be...
+
 // 10
+// 30
 // 20
+// 628
